dcab5244f6ce9faf9f6bd7ab2d64d50829c653f3
vBalatroni <valerio.balatroni@gmail.com>
2025-06-22 10:42:47 +0200

    file for production

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vueDevTools(),
     
   ],
-  base: '/outline_defekt_report/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
