a24696650d4f94f4c0efef6912f1fb94f7aace6a
Bahl <valerio@Mac.fritz.box>
2025-09-09 22:12:26 +0200

    wip fixes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,15 +3,8 @@ set -e
 
 echo "Starting backend..."
 
-if [ -n "$DATABASE_URL" ]; then
-  echo "Waiting for database..."
-  for i in $(seq 1 30); do
-    node -e "const { Client } = require('pg'); const c = new Client({ connectionString: process.env.DATABASE_URL }); c.connect().then(()=>process.exit(0)).catch(()=>process.exit(1));" && break || sleep 2
-  done
-fi
-
-echo "Running prisma migrate deploy..."
-npx prisma migrate deploy
+# Optional small wait to give Postgres time to accept connections
+sleep 3
 
 node dist/main.js
 
